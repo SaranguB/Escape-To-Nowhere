0f1f04ae55625592b8598516cdc5685a63ee6174
Sarangu Baisil Sanal <sarangubaisil8281@gmail.com>
2025-05-08 12:33:27 +0530

    World Setup

---
+++ Project 1/Assets/Scripts/Main/GameService.cs
@@ -0,0 +1,14 @@
+using Utilities;
+
+namespace Main
+{
+    public class GameService : GenericMonoSingelton<GameService>
+    {
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+        }
+    }
+}
